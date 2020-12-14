e7cd577eb64fd4fd20978bc5a129285fbc2df394
DamSenViet <26016342+DamSenViet@users.noreply.github.com>
2020-12-14 03:16:15 -0800

    core: added switch module and class

--- damsenviet/kle/__init__.py
+++ damsenviet/kle/__init__.py
@@ -3,6 +3,7 @@ __all__ = [
     "Metadata",
     "Background",
     "Key",
+    "Switch",
     "Label",
     "DeserializeException",
 ]
@@ -10,5 +11,6 @@ from .keyboard import Keyboard
 from .metadata import Metadata
 from .background import Background
 from .key import Key
+from .switch import Switch
 from .label import Label
 from .exceptions import DeserializeException
